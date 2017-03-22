4667aaf0ddee247363dd9e09f71e325b6a05ea92
nathaliaceballoshurtado <invitado@s5pc20.eisc.univalle.edu.co>
2017-03-22 16:57:10 -0500

    primer commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "util.h"
+
+int main(int c, char **argv){
+
+printf("%d\n", filesize(argv[1]));
+
+return 0;
+
+}
---
+++ util.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+
+
+    int filesize(char *filename) {
+        // Definicion e inicializacion de variables
+        FILE *fp;
+        int count = 0;
+
+        // Se abre el archivo y se contara caracter por caracter el numero 
+        // de bytes del archivo
+        fp = fopen(filename,"r");
+        if (fp == NULL) {
+                perror("filesize - No se pudo abrir archivo\n");
+                return -1;
+        }
+        while (fgetc(fp) != EOF) {
+                count++;
+        }
+        fclose(fp);
+        return count;
+}
